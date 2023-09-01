3aad3e70671f156f35fa55929c6df84db433b07f
JT Smith <nexxai@gmail.com>
2023-09-01 12:53:04 -0600

    Validating withdrawals

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ class Transaction {
   }
 
   commit() {
+    if (!this.isAllowed()) return false;
+
     // Keep track of the time of the transaction
     this.time = new Date();
     // Add the transaction to the account
     this.account.addTransaction(this);
+    return true;
   }
 }
 
@@ -18,11 +21,21 @@ class Withdrawal extends Transaction {
   get value() {
     return -this.amount;
   }
+
+  isAllowed() {
+    // If this transaction would cause the account to go into the negative,
+    // block it.
+    return this.account.balance - this.amount >= 0;
+  }
 }
 
 class Deposit extends Transaction {
   get value() {
     return this.amount;
+  }
+
+  isAllowed() {
+    return true;
   }
 }
 
@@ -50,18 +63,25 @@ class Account {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-const myAccount = new Account("snow-patrol");
+const myAccount = new Account();
 
-t1 = new Withdrawal(50.25, myAccount);
-t1.commit();
-console.log("Transaction 1:", t1);
+console.log("Starting Account Balance: ", myAccount.balance);
 
-t2 = new Withdrawal(9.99, myAccount);
-t2.commit();
-console.log("Transaction 2:", t2);
+console.log("Attempting to withdraw even $1 should fail...");
+const t1 = new Withdrawal(1.0, myAccount);
+console.log("Commit result:", t1.commit());
+console.log("Account Balance: ", myAccount.balance);
 
-t3 = new Deposit(120.0, myAccount);
-t3.commit();
-console.log("Transaction 3:", t3);
+console.log("Depositing should succeed...");
+const t2 = new Deposit(9.99, myAccount);
+console.log("Commit result:", t2.commit());
+console.log("Account Balance: ", myAccount.balance);
 
-console.log("Balance:", myAccount.balance);
+console.log("Withdrawal for 9.99 should be allowed...");
+const t3 = new Withdrawal(9.99, myAccount);
+console.log("Commit result:", t3.commit());
+
+console.log("Ending Account Balance: ", myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log("Account Transaction History: ", myAccount.transactions);
